7b308e13c156883bdd67280238aaf4296d48a133
Insia <insia.resistance@gmail.com>
2024-12-07 17:14:00 +0100

    analytics(

--- src/pages/4_analytics.py
+++ src/pages/4_analytics.py
@@ -11,6 +11,10 @@ st.title("Analytics")
 ammo_filter = []
 gear_filter = []
 position_filter = []
+start_date = pd.to_datetime("2023-01-01")
+end_date = pd.to_datetime("2025-12-31")
+metrics_container = st.empty()
+plot_container = st.empty()
 
 @st.cache_data
 def get_analytics():
@@ -32,28 +36,13 @@ def get_analytics():
 
 def filtering_data(df, ammo_filter, gear_filter, position_filter, start_date, end_date):
     logger.debug(f"Filtering data with {ammo_filter}, {gear_filter}, {position_filter}, {start_date}, {end_date}")
-
-    filtered_df = df.copy()
-
-    if ammo_filter:
-        filtered_df = filtered_df[filtered_df["name"].isin(ammo_filter)]
-
-    if gear_filter:
-        filtered_df = filtered_df[filtered_df["gear"].isin(gear_filter)]
-
-    if position_filter:
-        filtered_df = filtered_df[filtered_df["position"].isin(position_filter)]
-
-    if start_date is not None:
-        filtered_df = filtered_df.loc[filtered_df["created_at"] >= pd.to_datetime(start_date)]
-
-    if end_date is not None:
-        filtered_df = filtered_df.loc[filtered_df["created_at"] <= pd.to_datetime(end_date)]
-
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    df_copied = df.copy()
+    filtered_df = df_copied[(df_copied["name"].isin(ammo_filter)) & (df_copied["gear"].isin(gear_filter)) & (df_copied["position"].isin(position_filter)) & (df_copied["created_at"].between(start_date, end_date))]
+  
     mean_group_size = filtered_df["group_size"].mean()
-    with st.container(border = True) :
-        st.metric("Mean Group Size", value = f"{mean_group_size:.2f}")
-
+    
     st.write("Historic")
     filtered_df["created_at"] = pd.to_datetime(filtered_df["created_at"])  # Ensure datetime format
     chart = (
@@ -70,9 +59,12 @@ def filtering_data(df, ammo_filter, gear_filter, position_filter, start_date, en
                     title="Group Size Over Time"
                 )
             )
+    with plot_container:
+        with st.container(border = True) :
+            st.metric("Mean Group Size", value = f"{mean_group_size:.2f}")
 
-    with st.container(border = True):
-        st.altair_chart(chart, use_container_width=True)
+        with st.container(border = True):
+            st.altair_chart(chart, use_container_width=True)
 
     with st.expander("Details"):
         st.dataframe(filtered_df, use_container_width=True)
@@ -86,7 +78,7 @@ ammo_filter = st.sidebar.multiselect(
     "Select ammo",
     options=df["name"].unique(),
     default=df["name"].unique(),
-    #on_change = filtering_data
+    #on_change = filtering_data(df, ammo_filter, gear_filter, position_filter, start_date, end_date)
 
 )
 
@@ -112,5 +104,35 @@ start_date, end_date = st.sidebar.date_input(
 
 )
 
-filtering_data(df, ammo_filter, gear_filter, position_filter, start_date, end_date)
+# filtering_data(df, ammo_filter, gear_filter, position_filter, start_date, end_date)
+df_copied = df.copy()
+filtered_df = df_copied[(df_copied["name"].isin(ammo_filter)) & (df_copied["gear"].isin(gear_filter)) & (df_copied["position"].isin(position_filter)) & (df_copied["created_at"].between(pd.to_datetime(start_date), pd.to_datetime(end_date)))]
+  
+mean_group_size = filtered_df["group_size"].mean()
+logger.debug(f"Mean group size : {mean_group_size}")
+logger.debug(f"Filtered df : {filtered_df}")
+filtered_df["created_at"] = pd.to_datetime(filtered_df["created_at"])  # Ensure datetime format
+chart = (
+            alt.Chart(filtered_df)
+            .mark_line(point=True)
+            .encode(
+                x="created_at:T",
+                y="group_size:Q",
+                tooltip=["created_at", "group_size", "name", "gear"]
+            )
+            .properties(
+                width=800,
+                height=400,
+                title="Group Size Over Time"
+            )
+        )
+
+with metrics_container:
+    st.metric("Mean Group Size", value = f"{mean_group_size:.2f}")
+with plot_container:
+
+    st.altair_chart(chart, use_container_width=True)
+
+with st.expander("Details"):
+    st.dataframe(filtered_df, use_container_width=True)
 
