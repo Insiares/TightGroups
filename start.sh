d5a870989330189dc35c5a84ae572412a4f1510b
Insiares <133666385+Insiares@users.noreply.github.com>
2025-01-08 23:12:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Start the backend (FastAPI) in the background
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+python main.py &
 
 # Start the frontend (Streamlit)
 streamlit run src/main.py --server.port 8501 --server.address 0.0.0.0
