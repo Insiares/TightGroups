01990c4d1752816fa21ef8129a40a4acd94d5fa0
Insiares <133666385+Insiares@users.noreply.github.com>
2025-01-13 20:15:53 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 
 # Start the backend (FastAPI) in the background
 python main.py &
-
+until curl --output /dev/null --silent --head --fail http://0.0.0.0:8000/token; do
+    echo "Waiting for the backend to be ready..."
+    sleep 2
+done
 # Start the frontend (Streamlit)
 streamlit run src/main.py --server.port 8501 --server.address 0.0.0.0
